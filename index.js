05ac30fe6af8922c049d148a2b1281fde8e00860
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-06-20 12:22:20 +0200

    implement to/from function

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let textAreaEl = document.getElementById("text-field");
 let buttonEl = document.getElementById("publish-btn");
 let messageBoardEl = document.getElementById("message-board");
+let fromEl = document.getElementById("from");
+let toEl = document.getElementById("to");
 
 buttonEl.addEventListener("click", function () {
   let newMessage = textAreaEl.value;
+  let from = fromEl.value;
+  let to = toEl.value;
+
   messageBoardEl.innerHTML =
-    `<p class="message-txt">${newMessage}</p>` + messageBoardEl.innerHTML;
+    `<p class="message-txt bold">
+    To ${to} <br/><span class="inner">${newMessage}</span><br/>From ${from}</p>` +
+    messageBoardEl.innerHTML;
   textAreaEl.value = "";
+  fromEl.value = "";
+  toEl.value = "";
 });
